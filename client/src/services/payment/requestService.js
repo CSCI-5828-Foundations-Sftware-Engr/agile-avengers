e3ff92b2a51644d7b40cbde199a4e9fd052e4d90
Aishwarya Jayaramu <aija7428@colorado.edu>
2023-04-11 19:51:34 -0600

    Request Page

--- client/src/services/payment/requestService.js
+++ client/src/services/payment/requestService.js
@@ -8,6 +8,14 @@ const requestService = {
   getUserPermissions() {
     const url = `/api/v1/permission`;
     return axios.get(url, config);
+  },
+  getSenderList() {
+    const url = `${instanceUrl}/api/v1/get_sender_list`;
+    return axios.get(url, config);
+  },
+  makePayment(payload) {
+    const url = `${instanceUrl}/api/v1/request_payment`;
+    return axios.post(url, payload, config);
   }
 };
 
